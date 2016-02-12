742d677950bdb3eaf85ff4b204b6a18cd7a45503
JosepCh <josepchanza12@gmail.com>
2016-02-12 03:38:59 +0100

    Update randomicon.php

--- randomicon.php
+++ randomicon.php
@@ -1,7 +1,7 @@
 <?php
 
 $datosconexion = array(
-	"servername" => "13.80.23.136", 		// PON AQUI LA IP DE TU SERVIDOR
+	"servername" => "10.1.0.7", 		// PON AQUI LA IP DE TU SERVIDOR
 	"username" => "root", 			// PON AQUI EL USUARIO DE LA BD
 	"password" => "1212", 			// PON AQUI LA CONTRASEÑA DE LA BD
 	"dbname" => "log"
